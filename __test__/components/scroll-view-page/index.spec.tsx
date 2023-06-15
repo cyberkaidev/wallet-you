62f7c856c55b0a33dde3adba5de53158087484e8
Kaique Vinicius <cyberkaidev@gmail.com>
2023-06-15 00:59:16 -0300

    :rotating_light: new test

--- __test__/components/scroll-view-page/index.spec.tsx
+++ __test__/components/scroll-view-page/index.spec.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { ScrollViewPage } from '@src/components';
-import { render } from '@testing-library/react-native';
+import { act, render } from '@testing-library/react-native';
 import { View } from 'react-native';
 
-test('Render component', () => {
+describe('ScrollViewPage', () => {
 	const configTest = {
 		id: 'idScrollViewPage',
+		idRefreshControl: 'idRefreshControl',
+		refreshControl: () => true,
 	};
 
-	const { getByTestId } = render(
-		<ScrollViewPage>
-			<View />
-		</ScrollViewPage>,
-	);
+	test('Render component', () => {
+		const { getByTestId } = render(
+			<ScrollViewPage>
+				<View />
+			</ScrollViewPage>,
+		);
 
-	expect(getByTestId(configTest.id)).toBeTruthy();
+		expect(getByTestId(configTest.id)).toBeTruthy();
+	});
+
+	test('Refresh scroll', async () => {
+		const component = render(
+			<ScrollViewPage refreshControl={configTest.refreshControl}>
+				<View />
+			</ScrollViewPage>,
+		);
+
+		const scrollView = component.getByTestId(configTest.id);
+		expect(scrollView).toBeDefined();
+		const { refreshControl } = scrollView.props;
+		await act(async () => {
+			refreshControl.props.onRefresh();
+		});
+	});
 });
