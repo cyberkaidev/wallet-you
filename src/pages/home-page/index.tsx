3c03b87c9349fd674d81db4a64ac29e323fa35b4
Kaique Vinicius <cyberkaidev@gmail.com>
2023-06-15 00:56:21 -0300

    :sparkles: option to refresh the page

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -4,18 +4,25 @@ import { useFormatCurrency } from '@src/hooks';
 import { calculateBalance } from '@src/functions';
 import { PriceBitcoin, ScrollViewPage, TransactionList } from '@src/components';
 import { useBitcoinDataPrices, useUserData, useAppSettings } from '@src/stores';
+import { getBitcoinBalance } from '@src/services';
 
 export function HomePage() {
 	const { t } = useTranslation();
-	const { balance } = useUserData(state => state);
+	const { balance, key, fetchTransactions } = useUserData(state => state);
 	const { currency } = useAppSettings(state => state);
 	const { data, status } = useBitcoinDataPrices(state => state);
 	const currencyFormated = useFormatCurrency(
 		calculateBalance({ balance, currentPrice: data?.current_price[currency] }),
 	);
 
+	async function onRefresh() {
+		await getBitcoinBalance(key);
+		await fetchTransactions(key);
+		return true;
+	}
+
 	return (
-		<ScrollViewPage>
+		<ScrollViewPage refreshControl={() => onRefresh()}>
 			<PriceBitcoin
 				title={t('total-balance')}
 				price={currencyFormated}
