9afdeab35db264292aa499f85825e5ac4076c510
Kaique Vinicius <cyberkaidev@gmail.com>
2023-06-20 18:02:45 -0300

    :lipstick: removed border and resized components

--- src/routes/components/tab-bar/styles.ts
+++ src/routes/components/tab-bar/styles.ts
@@ -4,13 +4,10 @@ import { IndicatorProps } from './types';
 
 export const TabBarContainer = styled.View`
 	background-color: ${themes.colors.black_200};
-	border-top-width: 0.5px;
-	border-style: solid;
-	border-top-color: ${themes.colors.grey_300};
 `;
 
 export const SafeArea = styled.SafeAreaView`
-	height: 70px;
+	height: 60px;
 	width: 100%;
 	flex-direction: row;
 `;
@@ -24,7 +21,7 @@ export const Touch = styled.TouchableOpacity`
 export const Indicator = styled.View<IndicatorProps>`
 	background-color: ${({ isFocused }) => (isFocused ? themes.colors.purple_200 : 'transparent')};
 	width: 70px;
-	height: 35px;
+	height: 30px;
 	align-items: center;
 	justify-content: center;
 	border-radius: 100px;
