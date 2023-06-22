67fe89cd71198298d487fb69e86d6e8fddaab443
Kaique Vinicius <cyberkaidev@gmail.com>
2023-06-22 15:13:07 -0300

    :bug: adjusted padding on IOS

--- src/components/scroll-view-header-page/styles.ts
+++ src/components/scroll-view-header-page/styles.ts
@@ -1,7 +1,13 @@
 import { themes } from '@src/themes';
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 import { ScrollViewHeaderPageContainerProps } from './types';
 import Animated from 'react-native-reanimated';
+import Constants from 'expo-constants';
+
+const HEADER_HEIGHT = 115;
+const DEFAULT_MARGIN = 25;
+const STATUS_BAR = Constants.statusBarHeight;
 
 export const ScrollViewHeaderPageContainer = styled(
 	Animated.ScrollView,
@@ -9,7 +15,7 @@ export const ScrollViewHeaderPageContainer = styled(
 	flex: 1;
 	padding-left: ${({ horizontalPadding }) => horizontalPadding};
 	padding-right: ${({ horizontalPadding }) => horizontalPadding};
-	padding-top: 145px;
+	padding-top: ${(Platform.OS === 'ios' ? STATUS_BAR : 0) + HEADER_HEIGHT + DEFAULT_MARGIN}px;
 	background-color: ${themes.colors.black_300};
 `;
 
