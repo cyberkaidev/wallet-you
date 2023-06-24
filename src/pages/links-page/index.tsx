7322d8cee8a68401311bbb4c9f9c21dc7a2a6647
CyberKai <cyberkaidev@gmail.com>
2023-06-24 10:14:29 -0300

    :sparkles: added icons and Linking

--- src/pages/links-page/index.tsx
+++ src/pages/links-page/index.tsx
@@ -1,18 +1,22 @@
+import * as Linking from 'expo-linking';
 import React from 'react';
 
+import { IconGithub, IconTwitter } from '@/assets';
 import { ActionList, ScrollViewPage } from '@/components';
 
 export function LinksPage() {
 	const list = [
 		{
 			testID: 'idGithub',
+			prefixIcon: <IconGithub size={20} />,
 			title: '@cyberkaidev',
-			onAction: () => console.log(''),
+			onAction: () => Linking.openURL('https://github.com/cyberkaidev/wallet-you'),
 		},
 		{
 			testID: 'idTwitter',
+			prefixIcon: <IconTwitter size={20} />,
 			title: 'cyberkaidev/wallet-yout',
-			onAction: () => console.log(''),
+			onAction: () => Linking.openURL('https://twitter.com/cyberkaidev'),
 		},
 	];
 
