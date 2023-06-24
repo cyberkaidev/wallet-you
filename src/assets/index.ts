ae845083a63a53302a30386b2d82f821298192fb
CyberKai <cyberkaidev@gmail.com>
2023-06-24 10:07:38 -0300

    :sparkles: added new icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,10 +4,12 @@ import { IconArrowUp } from './icons/icon-arrow-up';
 import { IconBitcoin } from './icons/icon-bitcoin';
 import { IconCardano } from './icons/icon-cardano';
 import { IconChart } from './icons/icon-chart';
+import { IconGithub } from './icons/icon-github';
 import { IconHome } from './icons/icon-home';
 import { IconNoInternet } from './icons/icon-no-internet';
 import { IconSettings } from './icons/icon-settings';
 import { IconSuccess } from './icons/icon-success';
+import { IconTwitter } from './icons/icon-twitter';
 
 export {
 	IconArrowDown,
@@ -16,8 +18,10 @@ export {
 	IconBitcoin,
 	IconCardano,
 	IconChart,
+	IconGithub,
 	IconHome,
 	IconNoInternet,
 	IconSettings,
 	IconSuccess,
+	IconTwitter,
 };
