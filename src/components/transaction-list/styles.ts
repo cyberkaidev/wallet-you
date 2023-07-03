2e79fb18b906adcb9ab105bfb6fcce15664d749e
Kaique Vinicius <cyberkaidev@gmail.com>
2023-07-03 00:14:42 -0300

    :construction: work in progress

--- src/components/transaction-list/styles.ts
+++ src/components/transaction-list/styles.ts
@@ -4,30 +4,33 @@ import { themes } from '@/themes';
 
 import { ItemsProps } from './types';
 
+const { colors, border_radius, spaces } = themes;
+
 export const TransactionListContainer = styled.View`
-	margin-top: ${themes.spaces.space_25};
+	margin-top: ${spaces.space_25};
 `;
 
 export const TransactionContainer = styled.View`
-	border-radius: ${themes.border_radius.radius_10};
-	background-color: ${themes.colors.black_200};
+	border-radius: ${border_radius.radius_10};
+	background-color: ${colors.black_200};
 `;
 
 export const TransactionItemContainer = styled.TouchableOpacity`
 	flex: 1;
-	height: 85px;
 	flex-direction: row;
-	padding-left: ${themes.spaces.space_10};
+	padding-left: ${spaces.space_10};
 `;
 
 export const Items = styled.View<ItemsProps>`
+	flex: 1;
 	flex-direction: row;
 	align-items: center;
-	flex: 1;
-	padding-right: ${themes.spaces.space_10};
+	padding-top: ${spaces.space_25};
+	padding-bottom: ${spaces.space_25};
+	padding-right: ${spaces.space_10};
 	border-bottom-width: 1px;
 	border-bottom-color: ${({ borderVisible }) =>
-		borderVisible ? themes.colors.grey_200 : 'transparent'};
+		borderVisible ? colors.grey_200 : colors.transparent};
 	border-bottom-style: solid;
 `;
 
@@ -41,7 +44,7 @@ export const Circle = styled.View`
 	width: 50px;
 	justify-content: center;
 	align-items: center;
-	margin-right: ${themes.spaces.space_10};
+	margin-right: ${spaces.space_10};
 `;
 
 export const Center = styled.View`
@@ -52,5 +55,5 @@ export const Center = styled.View`
 
 export const Column = styled.View`
 	flex: 1;
-	margin-right: ${themes.spaces.space_15};
+	margin-right: ${spaces.space_15};
 `;
