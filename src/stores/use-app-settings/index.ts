39b46defe08e16975150746d8187f9eec8287286
Kaique Vinicius <cyberkaidev@gmail.com>
2023-07-03 11:08:35 -0300

    :sparkles: check if it's a tablet

--- src/stores/use-app-settings/index.ts
+++ src/stores/use-app-settings/index.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Device from 'expo-device';
 import i18n from 'i18next';
 import { create } from 'zustand';
 
@@ -9,6 +10,7 @@ import { UseAppSettingsProps } from './types';
 export const useAppSettings = create<UseAppSettingsProps>(set => ({
 	currency: 'usd',
 	language: 'en-us',
+	isTablet: null,
 	setCurrency: param => {
 		set({ currency: param });
 		AsyncStorage.setItem(storageKeys.currency, param);
@@ -17,5 +19,10 @@ export const useAppSettings = create<UseAppSettingsProps>(set => ({
 		set({ language: param });
 		AsyncStorage.setItem(storageKeys.language, param);
 		i18n.changeLanguage(param);
+	},
+	setIsTablet: async () => {
+		const deviceType = await Device.getDeviceTypeAsync();
+		const isTablet = Device.DeviceType[deviceType] == 'TABLET';
+		set({ isTablet: isTablet });
 	},
 }));
