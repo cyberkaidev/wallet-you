14892cdd71cab4d43cf930a8a7f7ea38a94c2621
Kaique Vinicius <cyberkaidev@gmail.com>
2023-07-18 12:04:07 -0300

    :rotating_light: added new mock

--- __test__/setup.ts
+++ __test__/setup.ts
@@ -6,6 +6,12 @@ import EN_US from '../src/translate/locales/en-us.json';
 import ES_AR from '../src/translate/locales/es-ar.json';
 import PT_BR from '../src/translate/locales/pt-br.json';
 
+jest.mock('react-native-safe-area-context', () => {
+	return {
+		useSafeAreaInsets: () => ({ bottom: 0 }),
+	};
+});
+
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
 
 jest.mock('@/hooks', () => {
