a56dde874b2b6f0d9344817f2c6d6bc419b4263e
CyberKai <cyberkaidev@gmail.com>
2023-08-27 10:07:27 -0300

    :truck: moving tests to your components

--- src/components/skeleton-loading/index.spec.tsx
+++ src/components/skeleton-loading/index.spec.tsx
@@ -4,9 +4,7 @@ import React from 'react';
 import { SkeletonLoading } from '@/components';
 
 test('Render component', () => {
-	const { getByTestId } = render(
-		<SkeletonLoading widthPorcent="10%" heightPorcent="10%" radius={0} />,
-	);
+	const { getByTestId } = render(<SkeletonLoading heightPorcent="10%" radius={0} />);
 
 	expect(getByTestId('idSkeletonLoading')).toBeTruthy();
 });
