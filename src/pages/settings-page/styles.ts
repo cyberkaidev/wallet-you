0492410d2521ac5c4860cc2b28aa5e432532f9ca
CyberKai <cyberkaidev@gmail.com>
2023-09-03 09:51:57 -0300

    :fire: removed styles

--- src/pages/settings-page/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-
-import { themes } from '@/themes';
-
-export const ContainerVersion = styled.View`
-	width: 100%;
-	max-width: ${themes.width.max_width};
-	align-self: center;
-`;
