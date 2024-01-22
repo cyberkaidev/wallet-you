426e44f2a9157188823fc7506e24609c605a459a
Kaique Vinicius <cyberkaidev@gmail.com>
2024-01-22 15:08:43 -0300

    :hammer: adjusted paddings

--- src/components/ScrollView.tsx
+++ src/components/ScrollView.tsx
@@ -1,14 +1,20 @@
+import Constants from 'expo-constants';
 import React from 'react';
+import { Platform } from 'react-native';
 import { RefreshControl, ScrollView as ScrollViewContainer } from 'react-native-gesture-handler';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { colors } from '@/helpers/themes';
+import { colors, spaces } from '@/helpers/themes';
 import { ScrollViewProps } from '@/types/ScrollViewType';
 
 export function ScrollView({ children, refreshControl }: ScrollViewProps) {
 	const [refreshing, setRefreshing] = React.useState(false);
 	const insets = useSafeAreaInsets();
+
+	const HEIGHT_STATUSBAR = React.useMemo(() => {
+		return Platform.OS === 'android' ? 0 : Constants.statusBarHeight;
+	}, []);
 
 	async function onRefresh() {
 		setRefreshing(true);
@@ -37,8 +43,8 @@ export function ScrollView({ children, refreshControl }: ScrollViewProps) {
 			alwaysBounceVertical
 			contentContainerStyle={{
 				paddingBottom: hp('3%') + insets.bottom,
-				paddingTop: 25,
-				paddingHorizontal: 15,
+				paddingTop: spaces.space_25 + HEIGHT_STATUSBAR,
+				paddingHorizontal: spaces.space_15,
 			}}
 			endFillColor={colors.transparent}
 			refreshControl={refreshController()}
