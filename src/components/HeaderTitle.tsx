4efdae5362e40408c4ac50da89b100d0e250a407
Kaique Vinicius <cyberkaidev@gmail.com>
2024-01-22 11:30:02 -0300

    :sparkles: created HeaderTitle

---
+++ src/components/HeaderTitle.tsx
@@ -0,0 +1,40 @@
+import Constants from 'expo-constants';
+import React, { useMemo } from 'react';
+import { Platform, StyleSheet, View } from 'react-native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+import { colors, spaces } from '@/helpers/themes';
+import { HeaderTitleProps } from '@/types/HeaderTitleType';
+
+import { Text } from './Text';
+
+export function HeaderTitle({ title }: HeaderTitleProps) {
+	const useHeightHeader = useMemo(() => {
+		const HEIGHT_STATUS_BAR = Platform.OS === 'ios' ? Constants.statusBarHeight : 0;
+		return hp('8%') + HEIGHT_STATUS_BAR;
+	}, []);
+
+	return (
+		<View testID="idHeaderTitle" style={[styles.container, { height: useHeightHeader }]}>
+			<View style={styles.content}>
+				<Text size="xxl" weight="bold">
+					{title}
+				</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		width: '100%',
+		backgroundColor: colors.black_000,
+		justifyContent: 'flex-end',
+	},
+	content: {
+		width: '100%',
+		height: hp('8%'),
+		paddingHorizontal: spaces.space_15,
+		justifyContent: 'center',
+	},
+});
