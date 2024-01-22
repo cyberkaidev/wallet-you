e637e2e6e9526521255e1bb618ce662981eb2dda
Kaique Vinicius <cyberkaidev@gmail.com>
2024-01-22 17:51:11 -0300

    :sparkles: created new components

---
+++ src/assets/icons/IconArrowUpRight.android.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import Svg, { Path } from 'react-native-svg';
+
+import { colors } from '@/helpers/themes';
+import { IconProps } from '@/types/IconType';
+
+export function IconArrowUpRight({ color = colors.white, porcentSize = '6%' }: IconProps) {
+	return (
+		<Svg width={wp(porcentSize)} height={wp(porcentSize)} viewBox="0 0 18 18" fill="none">
+			<Path
+				d="M1.93846 18L0 16.0615L13.2923 2.76923H1.38462V0H18V16.6154H15.2308V4.70769L1.93846 18Z"
+				fill={color}
+			/>
+		</Svg>
+	);
+}
