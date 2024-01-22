a65354d43473c3676d9f5b14bbfecf4e455bc10f
Kaique Vinicius <cyberkaidev@gmail.com>
2024-01-22 15:04:52 -0300

    :sparkles: created ButtonIcon

---
+++ src/components/ButtonIcon.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { ButtonIconProps } from '@/types/ButtonIconType';
+
+import { ButtonIconPlatform } from './fragments/ButtonIconPlatform';
+
+export function ButtonIcon({
+	testID = 'idButtonIcon',
+	children,
+	onPress,
+	disabled = false,
+	marginT,
+	marginB,
+	marginR,
+	marginL,
+}: ButtonIconProps) {
+	return (
+		<ButtonIconPlatform
+			testID={testID}
+			onPress={() => {
+				if (!disabled) onPress();
+			}}
+			disabled={disabled}
+			marginT={marginT}
+			marginB={marginB}
+			marginR={marginR}
+			marginL={marginL}
+		>
+			{children}
+		</ButtonIconPlatform>
+	);
+}
