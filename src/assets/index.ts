71a580fcfc54264f6bed1cd028ec2d5e3b891fb8
Kaique Vinicius <cyberkaidev@gmail.com>
2024-01-23 10:29:58 -0300

    :hammer: refactored imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,12 +4,8 @@ import { IconArrowRight } from './icons/IconArrowRight';
 import { IconArrowUpRight } from './icons/IconArrowUpRight';
 import { IconBitcoin } from './icons/IconBitcoin';
 import { IconChart } from './icons/IconChart';
-import { IconGithub } from './icons/IconGithub';
-import { IconHome } from './icons/IconHome';
 import { IconNoInternet } from './icons/IconNoInternet';
 import { IconSettings } from './icons/IconSettings';
-import { IconSuccess } from './icons/IconSuccess';
-import { IconTwitter } from './icons/IconTwitter';
 
 export {
 	IconArrowDownLeft,
@@ -18,10 +14,6 @@ export {
 	IconArrowUpRight,
 	IconBitcoin,
 	IconChart,
-	IconGithub,
-	IconHome,
 	IconNoInternet,
 	IconSettings,
-	IconSuccess,
-	IconTwitter,
 };
