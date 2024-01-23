dd903887e1fd1cd6daa13f3ff948c522a6af420e
Kaique Vinicius <cyberkaidev@gmail.com>
2024-01-23 17:28:50 -0300

    :wrench: changed days=7 to days=2

--- src/services/getBitcoinHistoricalPrice.ts
+++ src/services/getBitcoinHistoricalPrice.ts
@@ -5,7 +5,7 @@ import { ResponseData } from '@/types/GetBitcoinHistoricalPriceType';
 export async function getBitcoinHistoricalPrice() {
 	try {
 		const { data }: ResponseData = await axios.get(
-			'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=USD&days=7',
+			'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=USD&days=2',
 			{
 				headers: {
 					'Content-Type': 'application/json',
