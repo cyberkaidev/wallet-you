32f0e80ff79a7b00ffefa6ca920a82228fbc032e
cyberkaidev <cyberkaidev@gmail.com>
2024-02-06 09:24:16 -0300

    :lipstick: created interface

--- src/pages/NoInternetPage.tsx
+++ src/pages/NoInternetPage.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet, View } from 'react-native';
+
+import { IconNoInternet } from '@/assets';
+import { Text } from '@/components/Text';
+import { spaces } from '@/helpers/themes';
 
 export function NoInternetPage() {
-	return <View />;
+	const { t } = useTranslation();
+
+	return (
+		<View style={styles.container}>
+			<IconNoInternet porcentSize="10%" />
+			<Text marginT={spaces.space_15} size="l" weight="bold">
+				{t('no_internet_connection')}
+			</Text>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
