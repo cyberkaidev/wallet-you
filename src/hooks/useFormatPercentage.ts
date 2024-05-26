f88c6d4bd6a8ece17127adc942f31ef6fd36911d
cyberkaidev <cyberkaidev@gmail.com>
2024-05-26 10:30:18 -0300

    :fire: removed files

--- src/hooks/useFormatPercentage.ts
+++
@@ -1,7 +0,0 @@
-export function useFormatPercentage(value: number) {
-	const prefix = value < 0 ? '' : '+ ';
-	return {
-		percent: `${prefix}${value.toFixed(2)}%`,
-		isPositive: value > 0,
-	};
-}
