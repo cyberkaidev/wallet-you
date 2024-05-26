940dda0671d12c309d0152d0a09ad2b87aba5a06
cyberkaidev <cyberkaidev@gmail.com>
2024-05-26 10:36:05 -0300

    :fire: removed props

--- src/types/PriceBitcoinType.ts
+++ src/types/PriceBitcoinType.ts
@@ -1,8 +1,6 @@
 export interface PriceBitcoinProps {
 	title: string;
 	price: string;
-	porcent: string;
-	isPositive: boolean;
 	bitcoin: string;
 	status: 'loading' | 'success' | 'failed' | null;
 }
