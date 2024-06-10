bf640a2c81ce988c07b885e010d091c71b367f70
cyberkaidev <cyberkaidev@gmail.com>
2024-06-09 21:31:15 -0300

    :sparkles: added Chart component and cryptoToCurrency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,24 +2,25 @@ import React from 'react';
 
 import { LimitedWidthContainer } from '@/components/LimitedWidthContainer';
 import { ScrollView } from '@/components/ScrollView';
-import { calculateBalance } from '@/functions/calculateBalance';
-import { useFormatCurrency } from '@/hooks/useFormatCurrency';
+import { cryptoToCurrency } from '@/functions/convertCurrency';
 import { TransactionList } from '@/pages/fragments/TransactionList';
 import { getBitcoinBalance } from '@/services/getBitcoinBalance';
 import { useAppSettings } from '@/stores/useAppSettings';
 import { useBitcoinDataPrices } from '@/stores/useBitcoinDataPrices';
 import { useUserData } from '@/stores/useUserData';
 
+import { Chart } from './fragments/Chart';
 import { HeaderIcons } from './fragments/HeaderIcons';
 import { MyBitcoinPrice } from './fragments/MyBitcoinPrice';
 
 export function HomePage() {
 	const { balance, key, fetchTransactions } = useUserData();
 	const { currency } = useAppSettings();
-	const { data, status } = useBitcoinDataPrices();
-	const currencyFormated = useFormatCurrency(
-		calculateBalance({ balance, currentPrice: data?.current_price[currency] }),
-	);
+	const { currentPrice, status } = useBitcoinDataPrices();
+	const currencyFormated = cryptoToCurrency({
+		balance,
+		cryptoCurrentPrice: currentPrice?.[currency],
+	});
 
 	async function onRefresh() {
 		await getBitcoinBalance(key);
@@ -32,6 +33,7 @@ export function HomePage() {
 			<LimitedWidthContainer>
 				<HeaderIcons />
 				<MyBitcoinPrice price={currencyFormated} balance={balance} status={status} />
+				<Chart price={currentPrice?.[currency]} />
 				<TransactionList />
 			</LimitedWidthContainer>
 		</ScrollView>
