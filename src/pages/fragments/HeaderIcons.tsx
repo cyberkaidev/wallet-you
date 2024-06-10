8ba9df36ff134fecb13ca59bf9322e8a6a149be7
cyberkaidev <cyberkaidev@gmail.com>
2024-06-09 21:30:11 -0300

    :lipstick: removed ButtonIcon and added center Text

--- src/pages/fragments/HeaderIcons.tsx
+++ src/pages/fragments/HeaderIcons.tsx
@@ -1,24 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { StyleSheet, View } from 'react-native';
 
-import { IconChart, IconSettings } from '@/assets';
+import { IconSettings } from '@/assets';
 import { ButtonIcon } from '@/components/ButtonIcon';
+import { Text } from '@/components/Text';
 import { statusBarHeight } from '@/helpers/statusBarHeight';
-import { colors, spaces } from '@/helpers/themes';
+import { colors } from '@/helpers/themes';
 
 export function HeaderIcons() {
 	const navigation = useNavigation();
+	const { t } = useTranslation();
 
 	return (
 		<View style={styles.container}>
-			<ButtonIcon
-				testID="idChart"
-				onPress={() => navigation.navigate('ChartPage')}
-				marginR={spaces.space_25}
-			>
-				<IconChart color={colors.light_cyan} />
-			</ButtonIcon>
+			<View style={styles.center}>
+				<Text size="xl" weight="medium">
+					{t('my-wallet')}
+				</Text>
+			</View>
 			<ButtonIcon testID="idSettings" onPress={() => navigation.navigate('SettingsPage')}>
 				<IconSettings color={colors.light_cyan} />
 			</ButtonIcon>
@@ -28,8 +29,16 @@ export function HeaderIcons() {
 
 const styles = StyleSheet.create({
 	container: {
+		position: 'relative',
 		flexDirection: 'row',
 		justifyContent: 'flex-end',
+		alignItems: 'center',
 		marginTop: statusBarHeight,
+	},
+	center: {
+		position: 'absolute',
+		alignItems: 'center',
+		left: 0,
+		right: 0,
 	},
 });
