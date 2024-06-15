3094e355887aa5da35d6c58836f430a6b92b7221
cyberkaidev <cyberkaidev@gmail.com>
2024-06-15 16:55:38 -0300

    :hammer: removed switch and isTablet and added fontSizes themes

--- src/functions/schemeText.ts
+++ src/functions/schemeText.ts
@@ -1,6 +1,7 @@
+import { fontSizes } from '@/helpers/themes';
 import { SchemeTextProps } from '@/types/SchemeTextType';
 
-export function schemeText({ weight, size, isTablet }: SchemeTextProps) {
+export function schemeText({ weight, size }: SchemeTextProps) {
 	function onFontFamily() {
 		switch (weight) {
 			case 'regular':
@@ -15,31 +16,7 @@ export function schemeText({ weight, size, isTablet }: SchemeTextProps) {
 	}
 
 	function onFontSize() {
-		switch (size) {
-			case 'xs':
-				return '2.5%';
-			case 's':
-				if (isTablet) return '2.5%';
-				return '3%';
-			case 'm':
-				if (isTablet) return '3%';
-				return '3.4%';
-			case 'l':
-				if (isTablet) return '3.4%';
-				return '4.5%';
-			case 'xl':
-				if (isTablet) return '4.5%';
-				return '5.5%';
-			case 'xxl':
-				if (isTablet) return '5.5%';
-				return '6.5%';
-			case 'xxxl':
-				if (isTablet) return '6.5%';
-				return '10%';
-			default:
-				if (isTablet) return '3%';
-				return '3.4%';
-		}
+		return fontSizes[size];
 	}
 
 	return {
